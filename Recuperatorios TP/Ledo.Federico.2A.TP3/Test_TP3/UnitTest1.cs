caee1d8e2f78e4c8abc3ec8cb3b596c641283cea
federicoledo <federicoledo13@gmail.com>
2016-12-07 02:09:30 -0300

    Add files via upload

---
+++ Recuperatorios TP/Ledo.Federico.2A.TP3/Test_TP3/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Excepciones;
+
+namespace Test_TP3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDni()
+        {
+            try
+            {
+                Instructor instr = new Instructor(155, "Gonzalo", "Rodriguez", "TresCuatro751983", Persona.ENacionalidad.Argentino);
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+        }
+        [TestMethod]
+        public void TestNulos()
+        {
+            Instructor instr = new Instructor(155, "Gonzalo", "Rodriguez", "34751983", Persona.ENacionalidad.Argentino);
+            Assert.AreNotEqual(null, instr.ID);
+            Assert.AreNotEqual(null, instr.Nombre);
+            Assert.AreNotEqual(null, instr.Apellido);
+            Assert.AreNotEqual(null, instr.DNI);
+            Assert.AreNotEqual(null, instr.Nacionalidad);
+        }
+        [TestMethod]
+        public void TestGimnasioInstancia()
+        {
+            Gimnasio gimnasio = new Gimnasio();
+
+            Assert.AreNotEqual(null, gimnasio.LsAlumno);
+            Assert.AreNotEqual(null, gimnasio.LsInstr);
+            Assert.AreNotEqual(null, gimnasio.LsJorn);
+        }
+    }
+}
